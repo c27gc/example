76a21f06c86942fb6bfccf8c45d66a26240bcc51
Ricky20jg <ricardo20jgg@gmail.com>
2023-08-23 17:29:13 -0400

    puse dos lineas nuevas

--- hola_mundo.py
+++ hola_mundo.py
@@ -1,3 +1,6 @@
 print("Hola mundo!")
 print("Axel estuvo acá JAJAJA xD")
 print("Suena mejor \"Un pequeño paso para Axel, un gran salto para proyecto futuros\"")
+
+#Parte de Ricardo
+print("por aqui paso ricardo")
